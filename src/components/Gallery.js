55d2e82706401898c38172a027c2ce748d421b11
Nicolas Matter <nicolaspmatter@gmail.com>
2024-09-17 21:59:01 +0200

    refactor: NicoHead component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,18 +1,13 @@
+import { GalleryItem } from "./GalleryItem.js";
+import NicoHead from "./NicoHead.js";
+import { Overlay } from "./Overlay.js";
 import React from "react";
-import client from "../client";
-import imageUrlBuilder from "@sanity/image-url";
-
-import { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
-import nico5 from "../models/nico5.glb";
-import { Canvas } from "@react-three/fiber";
-import loadingGif from "../icons/loading-rippled.gif";
 import bigFilterIcon from "../icons/big.svg";
+import client from "../client.js";
+import imageUrlBuilder from "@sanity/image-url";
+import loadingGif from "../icons/loading-rippled.gif";
 import smallFilterIcon from "../icons/small.svg";
 import tileFilterIcon from "../icons/tiles.svg";
-//Import Components
-import { GalleryItem } from "./GalleryItem.js";
-import { Overlay } from "./Overlay.js";
 
 const builder = imageUrlBuilder(client);
 var waitToMove = false;
@@ -59,23 +54,23 @@ export class Gallery extends React.Component {
     let PARAMS = '*[_type=="projects"] | order(year desc)';
     client
       .fetch(PARAMS)
-      .then(response => {
+      .then((response) => {
         this.setState({
           data: response,
         });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
     PARAMS = '*[_type=="about"]';
     client
       .fetch(PARAMS)
-      .then(response => {
+      .then((response) => {
         this.setState({
           activeAbout: response,
         });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }
@@ -84,7 +79,7 @@ export class Gallery extends React.Component {
     document.removeEventListener("keydown", this.escFunction, false);
   }
 
-  setOverlay = data => {
+  setOverlay = (data) => {
     if (data === undefined) {
     } else {
       this.setState({ activeProject: data });
@@ -93,7 +88,7 @@ export class Gallery extends React.Component {
   };
 
   // Function: Change class of body element to switch color scheme
-  changeColor = string => {
+  changeColor = (string) => {
     let reset = false;
     usingColorScheme = true;
 
@@ -118,17 +113,25 @@ export class Gallery extends React.Component {
     });
   };
 
-  filterGallery = string => {
+  filterGallery = (string) => {
     // 1: Get  all objects which have the classname string we are looking for and put them in an array
     let array = document.getElementsByClassName("tag-button");
     let filteredArray = this.state.filteredProjects;
 
     for (var key in array) {
       let item = array[key];
-      if (item.classList && item.classList.contains(string) && !item.classList.contains("highlighted")) {
+      if (
+        item.classList &&
+        item.classList.contains(string) &&
+        !item.classList.contains("highlighted")
+      ) {
         filteredArray.push(item);
       }
-      if (item.classList && item.classList.contains(string) && item.classList.contains("highlighted")) {
+      if (
+        item.classList &&
+        item.classList.contains(string) &&
+        item.classList.contains("highlighted")
+      ) {
         var index = filteredArray.indexOf(item);
         if (index !== -1) {
           filteredArray.splice(index, 1);
@@ -138,7 +141,7 @@ export class Gallery extends React.Component {
     this.setState({ filteredProjects: filteredArray });
   };
 
-  showHideGalleryItem = string => {
+  showHideGalleryItem = (string) => {
     let id = "gallery-item-" + string;
     let element = document.getElementById(id);
     if (this.state.filteredProjects.length > 0 && element) {
@@ -151,7 +154,7 @@ export class Gallery extends React.Component {
     } else return "";
   };
 
-  checkFilter = string => {
+  checkFilter = (string) => {
     let array = this.state.filteredProjects;
     for (var i = 0; i < array.length; i++) {
       if (array[i].classList.contains(string)) {
@@ -165,7 +168,7 @@ export class Gallery extends React.Component {
     toggleOverlay("overlay-up");
   };
 
-  handleMouseMove = event => {
+  handleMouseMove = (event) => {
     if (!waitToMove) {
       waitToMove = true;
       setTimeout(() => {
@@ -182,7 +185,7 @@ export class Gallery extends React.Component {
     }
   };
 
-  changeGalleryView = string => {
+  changeGalleryView = (string) => {
     this.setState({ galleryView: string });
   };
 
@@ -191,55 +194,86 @@ export class Gallery extends React.Component {
     for (var i = 0; i < this.state.activeFilters.length; i++) {
       activeFilters += " " + this.state.activeFilters[i];
     }
-    if (!this.state.data.length) {
+
+    {
       return (
         <>
           <div className="gallery-container" onMouseMove={this.handleMouseMove}>
-            <img className="loading-gif" src={loadingGif} alt="loading-gif"></img>
-          </div>
-          <Canvas
-            className="main-canvas"
-            onCreated={state => {
-              state.camera.fov = 30;
-            }}>
-            <ambientLight intensity={0.4} color={0xfcb38c} />
-            <pointLight position={[10, 10, 10]} intensity={1} />
-            <NicoModel mousePosition={this.state.coords} />
-          </Canvas>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <div className="gallery-container" onMouseMove={this.handleMouseMove}>
+            {!this.state.data.length && (
+              <>
+                <img
+                  className="loading-gif"
+                  src={loadingGif}
+                  alt="loading-gif"
+                ></img>
+              </>
+            )}
+
             {/* Filters for different views of gallery */}
             <div className="filter-container">
-              <span className="filter-button" onClick={() => this.changeGalleryView("list")}>
-                <img className="svg-filter" src={bigFilterIcon} alt="svg-filter"></img>
+              <span
+                className="filter-button"
+                onClick={() => this.changeGalleryView("list")}
+              >
+                <img
+                  className="svg-filter"
+                  src={bigFilterIcon}
+                  alt="svg-filter"
+                ></img>
               </span>
-              <span className="filter-button" onClick={() => this.changeGalleryView("small")}>
-                <img className="svg-filter" src={smallFilterIcon} alt="svg-filter"></img>
+              <span
+                className="filter-button"
+                onClick={() => this.changeGalleryView("small")}
+              >
+                <img
+                  className="svg-filter"
+                  src={smallFilterIcon}
+                  alt="svg-filter"
+                ></img>
               </span>
-              <span className="filter-button" onClick={() => this.changeGalleryView("tiles")}>
-                <img className="svg-filter" src={tileFilterIcon} alt="svg-filter"></img>
+              <span
+                className="filter-button"
+                onClick={() => this.changeGalleryView("tiles")}
+              >
+                <img
+                  className="svg-filter"
+                  src={tileFilterIcon}
+                  alt="svg-filter"
+                ></img>
               </span>
             </div>
             <div className="color-switch-container">
-              <span className="cc-1-button color-switch-button" onClick={() => this.changeColor("cc-1")}>
+              <span
+                className="cc-1-button color-switch-button"
+                onClick={() => this.changeColor("cc-1")}
+              >
                 1
               </span>
-              <span className="cc-2-button color-switch-button" onClick={() => this.changeColor("cc-2")}>
+              <span
+                className="cc-2-button color-switch-button"
+                onClick={() => this.changeColor("cc-2")}
+              >
                 2
               </span>
-              <span className="cc-3-button color-switch-button" onClick={() => this.changeColor("cc-3")}>
+              <span
+                className="cc-3-button color-switch-button"
+                onClick={() => this.changeColor("cc-3")}
+              >
                 3
               </span>
-              <span className="cc-4-button color-switch-button" onClick={() => this.changeColor("cc-4")}>
+              <span
+                className="cc-4-button color-switch-button"
+                onClick={() => this.changeColor("cc-4")}
+              >
                 4
               </span>
             </div>
-            <div className={"gallery " + this.state.galleryView + " " + activeFilters}>
-              {this.state.data.map(item => (
+            <div
+              className={
+                "gallery " + this.state.galleryView + " " + activeFilters
+              }
+            >
+              {this.state.data.map((item) => (
                 <GalleryItem
                   key={item.projectID}
                   data={item}
@@ -251,15 +285,12 @@ export class Gallery extends React.Component {
               ))}
             </div>
           </div>
-          <Canvas
-            className="main-canvas"
-            onCreated={state => {
-              state.camera.fov = 30;
-            }}>
-            <ambientLight intensity={0.4} color={0xfcb38c} />
-            <pointLight position={[10, 10, 10]} intensity={1} />
-            <NicoModel mousePosition={this.state.coords} />
-          </Canvas>
+
+          <NicoHead
+            coords={this.state.coords}
+            usingColorScheme={usingColorScheme}
+          />
+
           <Overlay
             sliderKey="overlay-right"
             name="overlay-container overlay-container-right"
@@ -277,7 +308,7 @@ export class Gallery extends React.Component {
 }
 
 // Activate Overlay by giving it a class with the direction it is supposed to move
-export const toggleOverlay = direction => {
+export const toggleOverlay = (direction) => {
   let main = document.getElementById("main-container");
   main.classList.toggle(direction);
 };
@@ -288,31 +319,3 @@ export const resetOverlay = () => {
   main.classList.remove("overlay-left");
   main.classList.remove("overlay-up");
 };
-
-export function NicoModel(props) {
-  const refMesh = useRef();
-  const { nodes, materials } = useGLTF(nico5);
-  const rot = [
-    0.7 + props.mousePosition[0].y / window.innerHeight,
-    0,
-    0.9 - props.mousePosition[0].x / window.innerWidth,
-  ];
-
-  return (
-    <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.head3d003.geometry}
-        material={materials["material_0.003"]}
-        rotation={rot}
-        position={[1, -1, 0]}
-        scale={0.01}
-        ref={refMesh}>
-        {usingColorScheme && <meshStandardMaterial wireframe color="white" />}
-      </mesh>
-    </group>
-  );
-}
-
-useGLTF.preload(nico5);
---
+++ src/components/NicoHead.js
@@ -0,0 +1,64 @@
+import React, { useRef } from "react";
+
+import { Canvas } from "@react-three/fiber";
+import nico5 from "../models/nico5.glb";
+import { useGLTF } from "@react-three/drei";
+
+useGLTF.preload(nico5);
+
+class NicoHead extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { coords, usingColorScheme } = this.props;
+    return (
+      <>
+        <Canvas
+          className="main-canvas"
+          onCreated={(state) => {
+            state.camera.fov = 30;
+          }}
+        >
+          <ambientLight intensity={0.4} color={0xfcb38c} />
+          <pointLight position={[10, 10, 10]} intensity={1} />
+          <NicoModel
+            usingColorScheme={usingColorScheme}
+            mousePosition={coords}
+          />
+        </Canvas>{" "}
+      </>
+    );
+  }
+}
+
+const NicoModel = (props) => {
+  const { usingColorScheme } = props;
+  const refMesh = useRef();
+  const { nodes, materials } = useGLTF(nico5);
+  const rot = [
+    0.7 + props.mousePosition[0].y / window.innerHeight,
+    0,
+    0.9 - props.mousePosition[0].x / window.innerWidth,
+  ];
+
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.head3d003.geometry}
+        material={materials["material_0.003"]}
+        rotation={rot}
+        position={[1, -1, 0]}
+        scale={0.01}
+        ref={refMesh}
+      >
+        {usingColorScheme && <meshStandardMaterial wireframe color="white" />}
+      </mesh>
+    </group>
+  );
+};
+
+export default NicoHead;
