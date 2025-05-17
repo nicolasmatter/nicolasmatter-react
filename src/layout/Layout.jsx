9c95ac3034bde9cdba7e2891ea40c10932c20577
nicolasmatter <nicolaspmatter@gmail.com>
2025-05-17 10:06:00 +0200

    feat: track mouse position and update WebGLHead coordinates

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,29 @@
+import React, { useEffect, useState } from "react";
+
 import Navigation from "../components/Navigation";
 import { Outlet } from "react-router-dom";
-import React from "react";
 import WebGLHead from "../components/WebGLHead";
 
 const Layout = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <div className="layout">
       <Navigation />
       <WebGLHead
-        coords={[{ x: window.innerWidth / 2, y: window.innerHeight / 2 }]}
+        coords={[{ x: mousePosition.x, y: mousePosition.y }]}
         usingColorScheme={
           window.matchMedia("(prefers-color-scheme: dark)").matches
         }
